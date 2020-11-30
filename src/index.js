1a7fd9c3489342d0a627aebea9a3fae04263c7af
Elen Hakobyan <elen@elen-pc>
2020-11-30 18:19:16 +0400

    grid

--- src/index.js
+++ src/index.js
@@ -1,42 +1,59 @@
-// Import Application class that is the main part of our PIXI project
-import { Application } from '@pixi/app'
+import * as PIXI from 'pixi.js';
 
-// In order that PIXI could render things we need to register appropriate plugins
-import { Renderer } from '@pixi/core' // Renderer is the class that is going to register plugins
 
-import { BatchRenderer } from '@pixi/core' // BatchRenderer is the "plugin" for drawing sprites
-Renderer.registerPlugin('batch', BatchRenderer)
+export class Game extends PIXI.Application {
+	constructor() {
+		super({
+			width: window.innerWidth,
+			height: window.innerHeight,
+		})
 
-import { TickerPlugin } from '@pixi/ticker' // TickerPlugin is the plugin for running an update loop (it's for the application class)
-Application.registerPlugin(TickerPlugin)
 
-// And just for convenience let's register Loader plugin in order to use it right from Application instance like app.loader.add(..) etc.
-import { AppLoaderPlugin } from '@pixi/loaders'
-Application.registerPlugin(AppLoaderPlugin)
 
-// Sprite is our image on the stage
-import { Sprite } from '@pixi/sprite'
+		document.body.appendChild(this.view);
+		this.loadAssets()
 
-// App with width and height of the page
-const app = new Application({
-	width: window.innerWidth,
-	height: window.innerHeight
-})
-document.body.appendChild(app.view) // Create Canvas tag in the body
+	}
 
-// Load the logo
-app.loader.add('logo', './assets/logo.png')
-app.loader.load(() => {
-	const sprite = Sprite.from('logo')
-	sprite.anchor.set(0.5) // We want to rotate our sprite relative to the center, so 0.5
-	app.stage.addChild(sprite)
+	loadAssets() {
+		this.loader.add("nkar", '/assets/logo.png')
+		this.loader.load(() => {
+			this.setRisezeListener();
+			this.createElement()
+		})
+	};
 
-	// Position the sprite at the center of the stage
-	sprite.x = app.screen.width * 0.5
-	sprite.y = app.screen.height * 0.5
+	createElement() {
 
-	// Put the rotating function into the update loop
-	app.ticker.add(delta => {
-		sprite.rotation += 0.02 * delta
-	})
-})
+		let w = window.innerWidth;
+		let h = window.innerHeight;
+		for (let i = 1; i <= 3; i++) {
+			for (let j = 1; j <= 3; j++) {
+				this.nkar = PIXI.Sprite.from('nkar')
+				if (w < 3 * this.nkar.width || h < 3 * this.nkar.height) {
+					if (w > h) {
+						this.nkar.scale.set(h / (3 * this.nkar.height))
+					} else {
+						this.nkar.scale.set(w / (3 * this.nkar.width))
+					}
+				}
+				this.nkar.anchor.set(0.5)
+				this.nkar.x = i * Math.floor(w / 3) - Math.floor(w / 6)
+				this.nkar.y = j * Math.floor(h / 3) - Math.floor(h / 6)
+				this.stage.addChild((this.nkar))
+			}
+
+		}
+
+	}
+	setRisezeListener() {
+
+		window.addEventListener("resize", () => {
+
+		});
+
+
+	}
+
+}
+new Game();
